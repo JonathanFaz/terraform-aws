e9cba4083f3b901875c3f367ffed991c0bcfbc50
Jonathan Guzman <jaguz.guzman@gmail.com>
2022-02-22 08:37:21 -0600

    feat(infra): add terraform modules

---
+++ modules/ec2/variables.tf
@@ -0,0 +1,12 @@
+variable "subnet_private_1" {}
+variable "subnet_public_1" {}
+
+variable "db_endpoint" {}
+variable "db_name" {}
+variable "db_username" {}
+variable "db_password" {}
+
+variable "key_name" {}
+
+variable "sg_pub_id" {}
+variable "sg_priv_id" {}
